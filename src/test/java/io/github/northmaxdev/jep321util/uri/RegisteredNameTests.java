ccf1243372696b30454fe2b28015d201ac712c10
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2023-03-15 11:34:16 +0200

    Add RegisteredName (WIP)

---
+++ src/test/java/io/github/northmaxdev/jep321util/uri/RegisteredNameTests.java
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: Unlicense
+
+package io.github.northmaxdev.jep321util.uri;
+
+import nl.jqno.equalsverifier.EqualsVerifier;
+import org.junit.jupiter.api.Test;
+
+class RegisteredNameTests {
+
+    @Test
+    void equalsAndHashCode() {
+        EqualsVerifier.forClass(RegisteredName.class)
+                .verify();
+    }
+}
