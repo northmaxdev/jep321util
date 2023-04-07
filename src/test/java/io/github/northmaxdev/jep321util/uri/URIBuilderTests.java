bcaa443795d33d89ce0233ddd9c282750b15ae0f
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2023-04-08 01:42:15 +0300

    Unit-test URIBuilder::build

--- src/test/java/io/github/northmaxdev/jep321util/uri/URIBuilderTests.java
+++ src/test/java/io/github/northmaxdev/jep321util/uri/URIBuilderTests.java
@@ -2,9 +2,9 @@
 
 package io.github.northmaxdev.jep321util.uri;
 
+import com.google.common.net.HostSpecifier;
 import nl.jqno.equalsverifier.EqualsVerifier;
 import nl.jqno.equalsverifier.Warning;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -12,24 +12,40 @@ import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.net.URI;
+import java.util.List;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 class URIBuilderTests {
 
-    @Disabled
     @ParameterizedTest
     @MethodSource("provideConfigsAndExpectedResults")
     @DisplayName("Builder configurations produce the correct URIs")
-    void configsAndExpectedResultsMatch(URIBuilder builder, URI expected) {
-        URI actual = builder.build();
+    void configsAndExpectedResultsMatch(URIBuilder builderConfig, Iterable<URI> possibleResults) {
+        URI actual = builderConfig.build();
 
-        assertThat(actual).isEqualTo(expected);
+        assertThat(actual).isIn(possibleResults);
     }
 
     Stream<Arguments> provideConfigsAndExpectedResults() {
-        return Stream.of(); /* TODO */
+        URIBuilder config1 = new URIBuilder(HostSpecifier.fromValid("example.com"))
+                .https()
+                .defaultPort()
+                .pathSegment("reports")
+                .param("year", 2010)
+                .param("month", 5);
+        Iterable<URI> config1PossibleResults = List.of(
+                URI.create("https://example.com/reports?year=2010&month=5"),
+                URI.create("https://example.com/reports?month=5&year=2010")
+        );
+
+        /* TODO: More cases */
+
+        return Stream.of(
+                arguments(config1, config1PossibleResults)
+        );
     }
 
     @Test
