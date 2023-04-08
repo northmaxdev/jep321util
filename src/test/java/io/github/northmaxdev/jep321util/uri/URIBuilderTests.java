f6b07af65d1467d5ebf88b93de961fb4287dae52
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2023-04-08 22:44:14 +0300

    Update URIBuilder unit tests as per previous refactorings

--- src/test/java/io/github/northmaxdev/jep321util/uri/URIBuilderTests.java
+++ src/test/java/io/github/northmaxdev/jep321util/uri/URIBuilderTests.java
@@ -2,7 +2,6 @@
 
 package io.github.northmaxdev.jep321util.uri;
 
-import com.google.common.net.HostSpecifier;
 import nl.jqno.equalsverifier.EqualsVerifier;
 import nl.jqno.equalsverifier.Warning;
 import org.junit.jupiter.api.DisplayName;
@@ -19,8 +18,6 @@ import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 class URIBuilderTests {
 
-    static final HostSpecifier DUMMY_HOST = HostSpecifier.fromValid("example.com");
-
     @ParameterizedTest
     @MethodSource("provideConfigsAndExpectedResults")
     @DisplayName("Builder configurations produce the correct URIs")
@@ -31,45 +28,32 @@ class URIBuilderTests {
     }
 
     Stream<Arguments> provideConfigsAndExpectedResults() {
-        URIBuilder config1 = new URIBuilder(DUMMY_HOST)
+        URIBuilder config1 = URIBuilder.withValidHost("example.com")
                 .https()
                 .defaultPort()
-                .pathSegment("reports")
-                .param("year", 2010)
-                .param("month", 5);
-        URI config1Expected = URI.create("https://example.com/reports?year=2010&month=5");
+                .pathSegment("foo")
+                .param("bar", 42)
+                .param("baz", true);
+        URI config1Expected = URI.create("https://example.com/foo?bar=42&baz=true");
 
-        URIBuilder config2 = new URIBuilder(DUMMY_HOST)
+        URIBuilder config2 = URIBuilder.withValidHost("example.com");
+        URI config2Expected = URI.create("https://example.com/");
+
+        URIBuilder config3 = URIBuilder.withLocalhost()
                 .http()
-                .port(8080)
-                .param("foo", true);
-        URI config2Expected = URI.create("http://example.com:8080/?foo=true");
+                .port(8080);
+        URI config3Expected = URI.create("http://localhost:8080/");
 
-        URIBuilder config3 = new URIBuilder(DUMMY_HOST);
-        URI config3Expected = URI.create("https://example.com/");
-
-        URIBuilder config4 = new URIBuilder(DUMMY_HOST)
-                .pathSegment("products")
-                .pathSegment("laptops")
-                .pathSegment(12345);
-        URI config4Expected = URI.create("https://example.com/products/laptops/12345");
-
-        URIBuilder config5 = new URIBuilder(DUMMY_HOST)
-                .pathSegment("search")
-                .param("query", "Что такое JVM?");
-        URI config5Expected = URI.create("https://example.com/search?query=%D0%A7%D1%82%D0%BE%20%D1%82%D0%B0%D0%BA%D0%BE%D0%B5%20JVM%3F");
-
-        URIBuilder config6 = new URIBuilder(DUMMY_HOST)
-                .pathSegment("заказы");
-        URI config6Expected = URI.create("https://example.com/%D0%B7%D0%B0%D0%BA%D0%B0%D0%B7%D1%8B");
+        URIBuilder config4 = URIBuilder.withLocalhost()
+                .pathSegment("поиск")
+                .param("q", "Джава");
+        URI config4Expected = URI.create("https://localhost/%D0%BF%D0%BE%D0%B8%D1%81%D0%BA?q=%D0%94%D0%B6%D0%B0%D0%B2%D0%B0");
 
         return Stream.of(
                 arguments(config1, config1Expected),
                 arguments(config2, config2Expected),
                 arguments(config3, config3Expected),
-                arguments(config4, config4Expected),
-                arguments(config5, config5Expected),
-                arguments(config6, config6Expected)
+                arguments(config4, config4Expected)
         );
     }
 
